6084d212825580cbfe5efc35edb074c99ea40910
Gautam <ganpatigautam08@gmail.com>
2023-03-11 01:57:49 +0530

    commit

---
+++ lib/home_page.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:note/sql_helper.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> _notes = [];
+
+  bool _isLoading = true;
+
+  void _refreshNotes() async {
+    final data = await SQLHelper.getItems();
+    setState(() {
+      _notes = data;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshNotes();
+  }
+
+  final _titleController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  Future<void> _addItem() async {
+    await SQLHelper.createItem(
+        _titleController.text, _descriptionController.text);
+
+    _refreshNotes();
+    _titleController.clear();
+    _descriptionController.clear();
+  }
+
+  Future<void> _updateItem(int id) async {
+    await SQLHelper.updateItem(
+        id, _titleController.text, _descriptionController.text);
+    _refreshNotes();
+    _titleController.clear();
+    _descriptionController.clear();
+  }
+
+  void _deleteItem(int id) async {
+    await SQLHelper.deleteItem(id);
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text('Deleted'),
+    ));
+    _refreshNotes();
+  }
+
+  void _showForm(int? id) async {
+    if (id != null) {
+      final existingNote = _notes.firstWhere((element) => element['id'] == id);
+      _titleController.text = existingNote['title'];
+      _descriptionController.text = existingNote['description'];
+    }
+
+    showModalBottomSheet(
+        elevation: 5,
+        isScrollControlled: true,
+        context: context,
+        builder: (_) => Container(
+              padding: EdgeInsets.only(
+                  top: 15,
+                  left: 15,
+                  right: 15,
+                  bottom: MediaQuery.of(context).viewInsets.bottom + 120),
+              decoration: const BoxDecoration(border: Border.symmetric()),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(hintText: 'Title'),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: _descriptionController,
+                    decoration: const InputDecoration(hintText: 'Note'),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (id == null &&
+                          _descriptionController.text.isNotEmpty) {
+                        await _addItem();
+                      }
+                      if (id != null) {
+                        await _updateItem(id);
+                      }
+                      _titleController.clear();
+                      _descriptionController.clear();
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(id == null ? 'Create New' : 'Update'),
+                  ),
+                ],
+              ),
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 5,
+        backgroundColor: Colors.green.shade700,
+        title: const Text('Notes'),
+        centerTitle: false,
+      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _notes.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: const EdgeInsets.only(top: 8, left: 5, right: 5),
+                  color: Colors.green.shade600,
+                  child: ListTile(
+                    title: Text(
+                      _notes[index]['title'],
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    subtitle: Text(_notes[index]['description'],
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600)),
+                    trailing: SizedBox(
+                      width: 100,
+                      child: Row(
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.edit),
+                            onPressed: () {
+                              _showForm(_notes[index]['id']);
+                            },
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () => _deleteItem(_notes[index]['id']),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }),
+      floatingActionButton: FloatingActionButton(
+        elevation: 15,
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+        onPressed: () {
+          _showForm(null);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
